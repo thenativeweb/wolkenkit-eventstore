cded06dd6ae53efb13aa0b203a8da26ae7a11611
René Viering <rene.viering@gmail.com>
2018-03-23 22:02:16 +0100

    Configure integration tests.

---
+++ test/integration/inmemory/integrationTests.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const env = require('../../helpers/env'),
+      getTestsFor = require('../getTestsFor');
+
+suite('inmemory/integration', function () {
+  this.timeout(10 * 1000);
+
+  getTestsFor({
+    type: 'inmemory',
+    url: env.POSTGRES_URL_INTEGRATION
+  });
+});
