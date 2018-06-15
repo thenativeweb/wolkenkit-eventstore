39e6e22c29c3d9e5e204fe93e75b04fbff723a95
Golo Roden <golo.roden@thenativeweb.io>
2018-06-15 11:27:17 +0200

    Use wolkenkit infrastructure Docker images.

--- test/integration/pre.js
+++ test/integration/pre.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const shell = require('shelljs');
+const oneLine = require('common-tags/lib/oneLine'),
+      shell = require('shelljs');
 
 const env = require('../shared/env'),
       waitForMaria = require('../shared/waitForMaria'),
@@ -10,11 +11,59 @@ const env = require('../shared/env'),
       waitForSqlServer = require('../shared/waitForSqlServer');
 
 const pre = async function () {
-  shell.exec('docker run -d -p 3308:3306 -e MYSQL_ROOT_PASSWORD=wolkenkit -e MYSQL_USER=wolkenkit -e MYSQL_PASSWORD=wolkenkit -e MYSQL_DATABASE=wolkenkit --name mariadb-integration mariadb:10.3.5 --bind-address=0.0.0.0');
-  shell.exec('docker run -d -p 27019:27017 --name mongodb-integration mongo:3.4.2');
-  shell.exec('docker run -d -p 3311:3306 -e MYSQL_ROOT_PASSWORD=wolkenkit -e MYSQL_USER=wolkenkit -e MYSQL_PASSWORD=wolkenkit -e MYSQL_DATABASE=wolkenkit --name mysql-integration mysql:5.7.21 --bind-address=0.0.0.0');
-  shell.exec('docker run -d -p 5434:5432 -e POSTGRES_USER=wolkenkit -e POSTGRES_PASSWORD=wolkenkit -e POSTGRES_DB=wolkenkit --name postgres-integration postgres:9.6.4-alpine');
-  shell.exec('docker run -d -p 1434:1433 -e ACCEPT_EULA=Y -e SA_PASSWORD=Wolkenkit123 --name sqlserver-integration microsoft/mssql-server-linux:2017-CU6');
+  shell.exec(oneLine`
+    docker run
+      -d
+      -p 3308:3306
+      -e MYSQL_ROOT_PASSWORD=wolkenkit
+      -e MYSQL_USER=wolkenkit
+      -e MYSQL_PASSWORD=wolkenkit
+      -e MYSQL_DATABASE=wolkenkit
+      --name mariadb-integration
+      mariadb:10.3.5
+      --bind-address=0.0.0.0
+  `);
+  shell.exec(oneLine`
+    docker run
+      -d
+      -p 27019:27017
+      -e MONGODB_DATABASE=wolkenkit
+      -e MONGODB_USER=wolkenkit
+      -e MONGODB_PASS=wolkenkit
+      --name mongodb-integration
+      thenativeweb/wolkenkit-mongodb:latest
+  `);
+  shell.exec(oneLine`
+    docker run
+      -d
+      -p 3311:3306
+      -e MYSQL_ROOT_PASSWORD=wolkenkit
+      -e MYSQL_USER=wolkenkit
+      -e MYSQL_PASSWORD=wolkenkit
+      -e MYSQL_DATABASE=wolkenkit
+      --name mysql-integration
+      mysql:5.7.21
+      --bind-address=0.0.0.0
+  `);
+  shell.exec(oneLine`
+    docker run
+      -d
+      -p 5434:5432
+      -e POSTGRES_DB=wolkenkit
+      -e POSTGRES_USER=wolkenkit
+      -e POSTGRES_PASSWORD=wolkenkit
+      --name postgres-integration
+      thenativeweb/wolkenkit-postgres:latest
+  `);
+  shell.exec(oneLine`
+    docker run
+      -d
+      -p 1434:1433
+      -e ACCEPT_EULA=Y
+      -e SA_PASSWORD=Wolkenkit123
+      --name sqlserver-integration
+      microsoft/mssql-server-linux:2017-CU6
+  `);
 
   await waitForMaria({ url: env.MARIA_URL_INTEGRATION });
   await waitForMongo({ url: env.MONGO_URL_INTEGRATION });
--- test/performance/pre.js
+++ test/performance/pre.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const shell = require('shelljs');
+const oneLine = require('common-tags/lib/oneLine'),
+      shell = require('shelljs');
 
 const env = require('../shared/env'),
       waitForMaria = require('../shared/waitForMaria'),
@@ -10,11 +11,59 @@ const env = require('../shared/env'),
       waitForSqlServer = require('../shared/waitForSqlServer');
 
 const pre = async function () {
-  shell.exec('docker run -d -p 3309:3306 -e MYSQL_ROOT_PASSWORD=wolkenkit -e MYSQL_USER=wolkenkit -e MYSQL_PASSWORD=wolkenkit -e MYSQL_DATABASE=wolkenkit --name mariadb-performance mariadb:10.3.5 --bind-address=0.0.0.0');
-  shell.exec('docker run -d -p 27020:27017 --name mongodb-performance mongo:3.4.2');
-  shell.exec('docker run -d -p 3312:3306 -e MYSQL_ROOT_PASSWORD=wolkenkit -e MYSQL_USER=wolkenkit -e MYSQL_PASSWORD=wolkenkit -e MYSQL_DATABASE=wolkenkit --name mysql-performance mysql:5.7.21 --bind-address=0.0.0.0');
-  shell.exec('docker run -d -p 5435:5432 -e POSTGRES_USER=wolkenkit -e POSTGRES_PASSWORD=wolkenkit -e POSTGRES_DB=wolkenkit --name postgres-performance postgres:9.6.4-alpine');
-  shell.exec('docker run -d -p 1435:1433 -e ACCEPT_EULA=Y -e SA_PASSWORD=Wolkenkit123 --name sqlserver-performance microsoft/mssql-server-linux:2017-CU6');
+  shell.exec(oneLine`
+    docker run
+      -d
+      -p 3309:3306
+      -e MYSQL_ROOT_PASSWORD=wolkenkit
+      -e MYSQL_USER=wolkenkit
+      -e MYSQL_PASSWORD=wolkenkit
+      -e MYSQL_DATABASE=wolkenkit
+      --name mariadb-performance
+      mariadb:10.3.5
+      --bind-address=0.0.0.0
+  `);
+  shell.exec(oneLine`
+    docker run
+      -d
+      -p 27020:27017
+      -e MONGODB_DATABASE=wolkenkit
+      -e MONGODB_USER=wolkenkit
+      -e MONGODB_PASS=wolkenkit
+      --name mongodb-performance
+      thenativeweb/wolkenkit-mongodb:latest
+  `);
+  shell.exec(oneLine`
+    docker run
+      -d
+      -p 3312:3306
+      -e MYSQL_ROOT_PASSWORD=wolkenkit
+      -e MYSQL_USER=wolkenkit
+      -e MYSQL_PASSWORD=wolkenkit
+      -e MYSQL_DATABASE=wolkenkit
+      --name mysql-performance
+      mysql:5.7.21
+      --bind-address=0.0.0.0
+  `);
+  shell.exec(oneLine`
+    docker run
+      -d
+      -p 5435:5432
+      -e POSTGRES_DB=wolkenkit
+      -e POSTGRES_USER=wolkenkit
+      -e POSTGRES_PASSWORD=wolkenkit
+      --name postgres-performance
+      thenativeweb/wolkenkit-postgres:latest
+  `);
+  shell.exec(oneLine`
+    docker run
+      -d
+      -p 1435:1433
+      -e ACCEPT_EULA=Y
+      -e SA_PASSWORD=Wolkenkit123
+      --name sqlserver-performance
+      microsoft/mssql-server-linux:2017-CU6
+  `);
 
   await waitForMaria({ url: env.MARIA_URL_PERFORMANCE });
   await waitForMongo({ url: env.MONGO_URL_PERFORMANCE });
